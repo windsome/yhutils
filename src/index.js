5706bf2746384f2d6b523c6ce76abbbdf549487e
windsome.feng <36081189@qq.com>
2020-08-02 14:44:50 +0800

    update wutils to yhutils

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// 获取文件名中除扩展名部分
+export { default as fileBasename } from './fileBasename';
+
+// dataUrl相关
+export { default as dataUrlName } from './dataUrlName';
+export { default as dataUrlOfFile } from './dataUrlOfFile';
+export { default as dataUrlToFile } from './dataUrlToFile';
+export { default as dataUrlToBlob } from './dataUrlToBlob';
+
+// 图片压缩转换相关
+export { default as imageDataUrlScale } from './imageDataUrlScale';
+export { default as imageFileScale } from './imageFileScale';
+export { default as imageFileScaleList } from './imageFileScaleList';
+
